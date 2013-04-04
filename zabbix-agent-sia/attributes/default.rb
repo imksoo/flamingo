dd0bd75b6163255a79e3b9d0ca13b1c7dad14443
root <root@FLAMINGO.local>
2013-04-05 00:53:02 +0900

    Modified zabbix-agent, mysql recipes

---
+++ zabbix-agent-sia/attributes/default.rb
@@ -0,0 +1,33 @@
+default["zabbix_agent"]["pid_file"] = "/var/run/zabbix/zabbix_agentd.pid"
+default["zabbix_agent"]["log_file"] = "/var/log/zabbix/zabbix_agentd.log"
+default["zabbix_agent"]["log_file_size"] = 0
+
+default["zabbix_agent"]["debug_level"] = 3
+
+default["zabbix_agent"]["source_ip"] = nil
+
+default["zabbix_agent"]["enable_remote_commands"] = nil #default:0(not allowed), 1:(allowed)
+default["zabbix_agent"]["log_remote_commands"] = nil #default:0(disabled), 1:(enabled)
+
+default["zabbix_agent"]["server"] = "10.3.12.105" #comma delimited IP addresses
+default["zabbix_agent"]["server_active"] = "10.3.12.105" #comma delimited IP:port list
+
+default["zabbix_agent"]["listen_port"] = 10050 #default:10050
+default["zabbix_agent"]["listen_ip"] = "0.0.0.0" #default:0.0.0.0
+default["zabbix_agent"]["start_agents"] = 3 #default:3
+
+default["zabbix_agent"]["refresh_active_checks"] = 120 #sec, default:120
+
+default["zabbix_agent"]["buffer_send"] = 5 #default:5
+default["zabbix_agent"]["buffer_size"] = 100 #default:100
+default["zabbix_agent"]["max_lines_per_second"] = 100 #default:100
+
+default["zabbix_agent"]["allow_root"] = 0 #default:0(do not allow), 1(allow)
+
+default["zabbix_agent"]["timeout"] = 3 #sec, default:3
+
+default["zabbix_agent"]["include"] = ["/etc/zabbix/zabbix_agent.d/"] 
+
+default["zabbix_agent"]["unsafe_user_parameters"] = 0 #default:0(do not allow), 1(allow)
+default["zabbix_agent"]["user_parameter"] = [] #Format: "<key>,<shell command>"
+
--- zabbix-agent-sia/recipes/default.rb
+++ zabbix-agent-sia/recipes/default.rb
@@ -16,3 +16,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+chef_tempdir = "/tmp/chef/zabbix"
+
+zabbix_packages =  [
+	"zabbix-2.0.5-1.el6.x86_64.rpm",
+	"zabbix-agent-2.0.5-1.el6.x86_64.rpm",
+	"zabbix-get-2.0.5-1.el6.x86_64.rpm",
+	"zabbix-sender-2.0.5-1.el6.x86_64.rpm",
+]
+
+directory "#{chef_tempdir}"
+
+zabbix_packages.each { |pkg|
+	cookbook_file "#{chef_tempdir}/#{pkg}" do
+		not_if "rpm -q zabbix-server"
+	end
+}
+
+zabbix_package_source = []
+zabbix_packages.each { |pkg| zabbix_package_source.push("#{chef_tempdir}/#{pkg}") }
+
+execute "install-zabbix-server" do
+	notifies :stop, "service[zabbix-agent]", :immediate
+	command "yum install -y #{zabbix_package_source.join(' ')}"
+	not_if "rpm -q zabbix-agent"
+end
+
+template "/etc/zabbix/zabbix_agentd.conf" do
+	mode "644"
+	owner "root"
+	group "root"
+	source "zabbix_agentd.conf.erb"
+	notifies :restart, "service[zabbix-agent]", :delayed
+end
+
+directory "/etc/zabbix/zabbix_agentd.d"
+
+service "zabbix-agent" do
+	action [:enable, :start]
+end
+
