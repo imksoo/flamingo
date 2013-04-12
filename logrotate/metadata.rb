e7e6a7152b4b423838de5a7d998ef0074326483c
Kirino Minato / @imksoo <root@FLAMINGO.local>
2013-04-13 00:29:01 +0900

    Add logrotate recipe.

---
+++ logrotate/metadata.rb
@@ -0,0 +1,7 @@
+name             'logrotate'
+maintainer       'Kiniro Minato / @imksoo'
+maintainer_email 'kirino.minato@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures logrotate'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
